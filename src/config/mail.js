d435db80aa7f8d50ed0fc2080b0488f204d669e4
Rafael Tapia <tapia@MacBook-Air-de-Rafael.local>
2020-02-09 12:25:25 -0300

    Criado .env para controle das variaveis

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '344364efd7d06b',
-    pass: '16431efacb67fc',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
