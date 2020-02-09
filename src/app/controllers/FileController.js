4971ef41486f8d702b3e14644d97d6a8dc226a41
Rafael Tapia <tapia@MacBook-Air-de-Rafael.local>
2020-02-09 00:09:07 -0300

    Adicionado rota pra arquivos

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,18 @@
+import File from '../models/File';
+
+class FileController {
+  async store(req, res) {
+    const { originalname: name, filename: path } = req.file;
+
+    console.log(name, path);
+
+    const file = await File.create({
+      name,
+      path,
+    });
+
+    res.json(file);
+  }
+}
+
+export default new FileController();
