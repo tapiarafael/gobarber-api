d2d87b8a612c86bcfd00c710657384139679f946
Rafael Tapia <tapia@MacBook-Air-de-Rafael.local>
2020-02-09 01:11:54 -0300

    Rota para listar schedules

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -24,6 +25,8 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
