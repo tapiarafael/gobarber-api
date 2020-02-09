0d5d1fdf77e1070150956dc3fd3001fc78739f92
Rafael Tapia <tapia@MacBook-Air-de-Rafael.local>
2020-02-09 00:23:28 -0300

    Rota para criar appointments

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
@@ -23,5 +24,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
